c40338394617e7992066ca3198d0ee57fef0a400
Brunoiit <francisco18car@gmail.com>
2023-06-22 21:21:30 -0500

    Proyecto kivy

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Finder(App):
+    def build(self):
+        return Label(text='Inicio del proyecto Kivy para localizador')
+
+if __name__ == '__main__':
+    Finder().run()
